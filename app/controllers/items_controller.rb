ae5662fcdc2bf063af9cc09ff413c08e8322b2a3
sazanami16 <73375655+sazanami16@users.noreply.github.com>
2020-12-30 01:22:08 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,8 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? && current_user.id != @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index if user_signed_in? && current_user.id != @item.user_id
   end
 end
